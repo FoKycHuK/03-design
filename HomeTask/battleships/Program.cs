35e82c7cc77d81bdf0c0ef0eb368c1232839c00b
FoKycHuK <fokychuk47@ya.ru>
2015-02-27 16:22:17 +0600

    removed useless factory, used simple variables

--- HomeTask/battleships/Program.cs
+++ HomeTask/battleships/Program.cs
@@ -18,16 +18,17 @@ namespace battleships
 				Console.WriteLine("Usage: {0} <ai.exe>", Process.GetCurrentProcess().ProcessName);
 				return;
 			}
-			var fabric = new Factory();
 			var aiPath = args[0];
+			Func<Map, Ai, Game> createGame = (map, ai) => new Game(map, ai);
+			Func<string, ProcessMonitor, Ai> createAi = (path, monitor) => new Ai(path, monitor);
 			var container = new StandardKernel();
 			container.Bind<Settings>().To<Settings>().WithConstructorArgument("settings.txt");
 			var settings = container.Get<Settings>();
 			container.Bind<AiTester>().To<AiTester>()
 				.WithConstructorArgument(aiPath);
 			container.Bind<Random>().ToConstant(new Random(settings.RandomSeed));
-			container.Bind<Func<Map, Ai, Game>>().ToMethod(ctx => ctx.Kernel.Get<Factory>().CreateGame);
-			container.Bind<Func<string, ProcessMonitor, Ai>>().ToMethod(ctx => ctx.Kernel.Get<Factory>().CreateAi);
+			container.Bind<Func<Map, Ai, Game>>().ToMethod(ctx => createGame);
+			container.Bind<Func<string, ProcessMonitor, Ai>>().ToMethod(ctx => createAi);
 			//var settings = new Settings("settings.txt");
 			//var logger = LogManager.GetLogger("results");
 			//var tester = new AiTester(settings, logger);
